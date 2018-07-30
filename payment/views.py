7b17e7616e0d1ac9c280231d42d7120404270e38
darchard1984 <32482170+darchard1984@users.noreply.github.com>
2018-07-30 23:36:26 +0100

    payment form details populating with client info

--- payment/views.py
+++ payment/views.py
@@ -10,13 +10,14 @@ from profiles.view_func import profile_exists
 def payment(request, username, cycle_id):
     total = get_object_or_404(Cycles, pk=cycle_id).cycle_value
     profile = profile_exists(request.user.pk)
+    payment = PaymentForm()
     if profile:
         profile_dict = model_to_dict(profile)
         name = '{0} {1}'.format(request.user.first_name, request.user.last_name)
         profile_dict['name_on_card'] = name
         payment = PaymentForm(profile_dict)
     else:
-        payment = PaymentForm(profile_dict)
+        payment = PaymentForm()
     return render(request, 
                 'payment.html', 
                 {'payment_form': payment,
