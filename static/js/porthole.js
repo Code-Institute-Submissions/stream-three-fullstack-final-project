ae0a2ad9c205acf761cdaaca682d53acb1b35bb4
darchard1984 <32482170+darchard1984@users.noreply.github.com>
2018-08-11 23:27:12 +0100

    styling file upload buttons

--- static/js/porthole.js
+++ static/js/porthole.js
@@ -6,6 +6,7 @@
     const input = document.getElementsByTagName('input');
     const selects = document.getElementsByTagName('select');
     const valueInput = document.getElementById('id_cycle_value_0');
+    const browseButtons = document.getElementsByClassName('browse-button__button');
 
     // GET INPUTS BY FILE INPUT TYPE //
     const getFileInput = (input) => {
@@ -16,15 +17,36 @@
 
             if (input[i].getAttribute('type') == 'file') {
 
-                fileInputs.push(input[i])
+                fileInputs.push(input[i]);
 
-                
             }
         }
        
-        return fileInputs
+        return fileInputs;
 
     }
+
+
+    // SWITCH OUT FILE INPUT ID'S FOR INDIVUAL STYLING //
+    const replaceFileInputId = (fileInput) => {
+
+        for (i = 0; i < fileInput.length; i++) {
+
+            fileInput[i].setAttribute('id', `upload-${i}`);
+        }
+    }
+
+    // HIDE UPLOAD FIELDS TO REPLACE WITH CUSTOM STYLED UPLOAD FIELDS //
+
+    const hideUploadFields = (uploadFields) => {
+       
+
+        for (i = 0; i < uploadFields.length; i++) {
+
+            uploadFields[i].style.display='none';
+        }
+    }
+
 
     // HIDE CURRENCY DROP DOWN LIST //
     const hideSelects = (selects) => {
@@ -36,19 +58,48 @@
     }
 
     // ADD PLACEHOLDER TO INPUT VALUE // 
-
     const addPlaceHolderToValueInput = () => {
 
         valueInput.setAttribute('placeholder', 'Enter cycle value')
     }
 
+     // TRIGGER HIDDEN UNSTYLED BROWSE BUTTON ON CLICK OF STYLED BUTTON //
+    const onBrowseButtonClickTriggerHiddenButton = (browseButtons) => {
+        console.log(browseButtons);
+        for (i = 0; i < browseButtons.length; i++) {
 
+            browseButtons[i].addEventListener('click', () => {
+
+                document.getElementById(`upload-${i}`).click();
+
+            });
+        }
+
+    }
+
+    //----------------------------- FUNCTION CALLS ------------------------// 
+
+    // REPLACE ID OF INPUT OF TYPE=FILE // 
     const fileInput = getFileInput(input);
+    replaceFileInputId(fileInput);
 
-    console.log(fileInput);
+    // GET THE ELEMENTS WITH REPLACED ID'S //
+    const quoteUpload = document.getElementById('upload-0');
+    const poUpload = document.getElementById('upload-1');
+    const invoiceUpload = document.getElementById('upload-2');
+    const uploadFields = [quoteUpload, poUpload, invoiceUpload];
 
+    // HIDE THOSE ELEMENTS // 
+    hideUploadFields(uploadFields);
+
+    // TRIGGER HIDDEN DJANGO GENERATED FORM BROWSE BUTTON //
+    onBrowseButtonClickTriggerHiddenButton(browseButtons);
+
+    // STYLE UPLOAD FORM //
     addPlaceHolderToValueInput(valueInput);
     hideSelects(selects);
+
+    // BODY AND NAV STYLES //
     addClassToClassList(nav, 'porthole-nav');
     addBgColorToBody('porthole-bg-color');
 
