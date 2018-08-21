94da1f8293ef0588b868d8c5d2397ec68d585041
darchard1984 <32482170+darchard1984@users.noreply.github.com>
2018-08-21 23:33:24 +0100

    tweaked Delete File signals to delete AWS files using Boto3

--- cycleporthole/signals.py
+++ cycleporthole/signals.py
@@ -1,6 +1,6 @@
 import os
 import boto3
-#from boto.s3.connection import S3Connection, Bucket, Key
+import botocore
 import shutil
 from fileo.settings import MEDIA_URL, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_S3_REGION_NAME
 from django.db import models
@@ -8,64 +8,31 @@ from django.http import Http404
 from django.dispatch import receiver
 from .models import Quotes, PurchaseOrder, Invoices
 
-
+## Receiver Helper Function ##
 def remove_file_from_aws(instance):    
     s3 = boto3.resource('s3')
-    obj = s3.Object('fileo', 'media/{0}'.format(str(instance.file)))
-    obj.delete()
-    
-## Delete Cycle File From Model on Deleting DB entry ##
-## Receiver Helper Function ##
+    try:
+        obj = s3.Object('fileo', 'media/{0}'.format(str(instance.file)))
+        obj.delete()
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            print('Error deleting file: {0}'.format(e))
+            raise Http404
 
-
-
-## If file exists, delete it, then delete folder structure ##
-def remove_file_on_delete_helper(instance, client_path):
-    if instance.file:
-        if os.path.isfile(instance.file.path):
-            try:
-                os.remove(instance.file.path)
-            except OSError as e:
-                print(e + 'error deleting file')
-                raise Http404
-                
-            try:
-                shutil.rmtree(client_path)
-            except OSError as e:
-                print(e + 'error deleting file directories')
-                raise Http404
 
 ## RECEIVERS ##
 ## Delete Cycle Quotes File on Deleting DB field ## 
 @receiver(models.signals.post_delete, sender=Quotes)
 def auto_delete_quote_file(sender, instance, **kwargs):
-    client_path='{0}/quote/{1}/{2}/{3}'.format(MEDIA_URL, 
-                                                instance.member,
-                                                instance.client,
-                                                instance.cycle.id)
-
+    remove_file_from_aws(instance)
     
-    remove_file_on_delete_helper(instance)
-    #remove_file_from_aws(instance)
-    
-
 ## Delete Cycle PO file on Deleting DB field ## 
 @receiver(models.signals.post_delete, sender=PurchaseOrder)
 def auto_delete_po_file(sender, instance, **kwargs):
-    client_path='{0}/po/{1}/{2}/{3}'.format(MEDIA_URL, 
-                                                instance.member,
-                                                instance.client,
-                                                instance.cycle.id)
-    if os.path.exists('env.py'):
-        remove_file_on_delete_helper(instance, client_path)
+    remove_file_from_aws(instance)
 
 ## Delete Cycle Invoice File on Deleting DB field ##
 @receiver(models.signals.post_delete, sender=Invoices)
 def auto_delete_invoice_file(sender, instance, **kwargs):
-    client_path='{0}/invoice/{1}/{2}/{3}'.format(MEDIA_URL, 
-                                                instance.member,
-                                                instance.client,
-                                                instance.cycle.id)
-    if os.path.exists('env.py'):
-        remove_file_on_delete_helper(instance, client_path)
+    remove_file_from_aws(instance)
 
