45db56582454946c305fdbea5481ea26e174cdcc
darchard1984 <32482170+darchard1984@users.noreply.github.com>
2018-08-25 17:26:46 +0100

    login jerkiness for iphone fixed

--- static/js/login.js
+++ static/js/login.js
@@ -2,14 +2,17 @@
 
 (() => {
 
+    const landingContainer = document.getElementById('landing-container');
+    const loginBlurbContainer = document.getElementById('login-blurb');
+    const upArrow = document.getElementById("up-arrow");
+    const downArrow = document.getElementById("down-arrow");
+  
+   
     // FUNCTION ROTATES ARROW ICON WHEN SCROLLING //
-    const body =  document.getElementsByTagName('body');
+    
     const rotateUpArrowOnScrollDown = () => {
 
         window.addEventListener('scroll', () => {
-
-            const upArrow = document.getElementById("up-arrow");
-            const downArrow = document.getElementById("down-arrow");
 
             if (window.scrollY > (upArrow.offsetTop + upArrow.offsetHeight)) {
         
@@ -25,17 +28,48 @@
         });
 
     }
+      
+    // SET FIXED HEIGHTS ON 100% CENTRALLY ALIGNED FIXED FLEX ELEMENT TO FIX JERKING ON SCROLL //
+    // LISTEN FOR ORIENTATION CHANGE TO RECALCULATE DIV HEIGHT //
+    const orientationValue = () => {
+        
+        window.addEventListener('orientationchange', () => {
 
-   //console.log(body);
-    let bodyHeight = (0.9 * $(window).height());
-   
-    bodyHeight = `${bodyHeight}` + 'px';
-    for ( let i = 0; i < body.lenght; i++ ) {
+            let value = window.orientation;
+           
+            if (value == 0) {
 
-        body[i].setAttribute('style', `height:${bodyHeight}`)
+                landingContainer.setAttribute('style', `height: ${(window.innerHeight).toString() + 'px'};` );
+                loginBlurbContainer.setAttribute('style', `height: ${(window.innerHeight + 60).toString() + 'px'};`);
 
+            } else if ( value != 0) {
+
+                landingContainer.setAttribute('style', `height: ${(window.innerHeight).toString() + 'px'};`);
+                loginBlurbContainer.removeAttribute('style');
+
+            }
+
+        })
     }
-    
+   
+    // FUNCTION CALLS  //
+
     rotateUpArrowOnScrollDown();
+
+    // DETECT IF MOBILE BROWSER // 
+
+    // RUN FUNCTION BELOW IF TRUE. FUNCTION COMBATES ISSUES WITH MOBILE BROWSER AUTO/SHOW HIDE OF URL BAR //
+    // AND THE JERKY DISPLAY OF FIXED DIVS //
+
+    // CREDIT : https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser // 
+
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    if (isMobile) {
+        landingContainer.setAttribute('style', `height: ${(window.innerHeight).toString() + 'px'};` );
+        loginBlurbContainer.setAttribute('style', `height: ${(window.innerHeight + 60).toString() + 'px'};`);
+        orientationValue();
+    }
+   
 
 })();
