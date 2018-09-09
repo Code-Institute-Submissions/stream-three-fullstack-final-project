7c1f90d87b93d2da0e0f5f1b1c8e797fdf5314a1
darchard1984 <32482170+darchard1984@users.noreply.github.com>
2018-09-09 16:24:05 +0100

    cycles table results table implemented

--- static/js/cycles.js
+++ static/js/cycles.js
@@ -2,12 +2,16 @@
   
 (() => {
 
-    const cycleStatus = document.getElementsByClassName('column-icon');
+   // const cycleStatus = document.getElementsByClassName('column-icon');
     const portholeButton = document.getElementsByClassName('cycles-results__porthole-button');
+
+    //-----//
+
+    const cycleStatus = document.getElementsByClassName(' cycles-status-icon');
 
     // -------------------------- FUNCTIONS --------------------------- //
 
-    // CHANGE THE COLOR OF THE RESULTS CONTAINER DIV AND STATUS ICON DEPENDING ON ITS STATUS //
+    // CHANGE THE COLOR OF THE RESULTS CONTAINER DIV AND STATUS ICON DEPENDING ON ITS STATUS FOR NON TABLE TABLES //
     const colorForStatus = (cycleStatus) => {
 
         if (cycleStatus) {
@@ -53,9 +57,55 @@
 
     }
 
+    const colorForStatusTable = () => {
+
+        if (cycleStatus) {
+        
+            for(let i = 0; i < cycleStatus.length; i ++){ 
+                // GET CYCLE STATUS CONTAINER //
+                const cycleStatusParent = cycleStatus[i].
+                                        parentElement.
+                                        parentElement.
+                                        parentElement.
+                                        parentElement.
+                                        parentElement;
+
+                // GET CYCLES STATUS FIRST ICON DATA ID //
+                const status = cycleStatus[i].getAttribute('data-id');
+
+                const statusText = cycleStatus[i].parentElement;
+                
+                // DEPENDING ON DATA ID SET COLOR //
+                if (status == "cancelled") {
+                   
+                    cycleStatusParent.classList.add('cycle-results__container-cancelled--color');
+                    statusText.classList.add('cycle-results__overall-icon-cancelled--color');
+    
+                } else if (status == "pending") {
+    
+                    cycleStatusParent.classList.add('cycle-results__container-pending--color');
+                    statusText.classList.add('cycle-results__overall-icon-pending--color');
+                   
+                } else if (status == "complete") {
+    
+                    cycleStatusParent.classList.add('cycle-results__container-complete--color');
+                    statusText.classList.add('cycle-results__overall-icon-complete--color');
+
+                } else if (status == "active") {
+
+                    statusText.classList.add('cycle-results__overall-icon-active--color');
+                }
+                
+            }
+            
+        }
+
+    }
+
     // -------------------- FUNCTION CALLS ----------------------- //
     
-    colorForStatus(cycleStatus);
+   //colorForStatus(cycleStatus);
+    colorForStatusTable();
 
     for (let i = 0; i < portholeButton.length; i++) {
 
