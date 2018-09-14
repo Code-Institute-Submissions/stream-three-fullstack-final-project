beb803c5b3243c0bebf4c5f4209448392d3a4f85
darchard1984 <32482170+darchard1984@users.noreply.github.com>
2018-09-14 15:38:39 +0100

    admin url removed from production

--- fileo/urls.py
+++ fileo/urls.py
@@ -31,7 +31,7 @@ from fileo.settings import MEDIA_ROOT
 
 urlpatterns = [
 
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
     url(r'^$', index, name='index'),
     url(r'^accounts/', include(urls_accounts)),
